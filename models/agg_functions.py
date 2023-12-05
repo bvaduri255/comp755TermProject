66b60b73aa1a97f94386a2a4af183a04658d01fb
aaravmehta <amehta@unc.edu>
2023-12-04 21:47:24 -0500

    added graphs

--- models/agg_functions.py
+++ models/agg_functions.py
@@ -42,3 +42,54 @@ def proximal_operator_weighted_average(parameters):
     # Apply proximal_operator to avg_params
     weights = proximal_operator(0.1, avg_params)
     return np.mean([w * x for w, x in zip(weights, parameters)], axis=0)
+
+
+accuracies= np.abs(predicitions - np.vstack(y_train_attack, y_test_attack))
+
+percent = (1- np.mean(accuracies))*100
+
+prediciton_squared = (np.multiply(predictions, predictions))
+
+0,2,5,6
+
+import matplotlib.pyplot as plt
+
+def plot_accuracy_privacy_graph(accuracy, privacy, labels):
+    """
+    Plots a graph with accuracy on the x-axis and privacy on the y-axis.
+
+    :param accuracy: List of accuracy values.
+    :param privacy: List of privacy values.
+    :param labels: List of labels for each point.
+    """
+    fig, ax = plt.subplots()
+
+    # Plot each point with its label
+    for i in range(len(accuracy)):
+        ax.scatter(accuracy[i], privacy[i])
+        ax.text(accuracy[i], privacy[i], labels[i], fontsize=9, ha='right')
+
+    # Adding 50% dotted lines to form quadrants
+    ax.axhline(y=50, color='gray', linestyle='--')
+    ax.axvline(x=50, color='gray', linestyle='--')
+
+    # Setting the range of axes from 0% to 100%
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+
+    # Labels for axes
+    ax.set_xlabel('Accuracy (%)')
+    ax.set_ylabel('Privacy (%)')
+
+    # Title of the graph
+    ax.set_title('Accuracy vs Privacy Graph')
+
+    plt.show()
+
+# Example data
+accuracy = [20, 40, 70, 90]
+privacy = [80, 60, 30, 10]
+labels = ['A', 'B', 'C', 'D']
+
+# Plotting the graph
+plot_accuracy_privacy_graph(accuracy, privacy, labels)
